8b3aad827812dea2efb19083c6eb01ecedfdae28
Zizhang He <hzz1996@qq.com>
2024-02-25 16:38:12 -0600

    Create a edit expense feature and manage app-wide context

--- App.js
+++ App.js
@@ -1,26 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
 import ManageExpense from './screens/ManageExpense';
 import RecentExpenses from './screens/RecentExpenses';
 import AllExpenses from './screens/AllExpenses';
+import { GlobalStyles } from './constants/styles';
+import IconButton from './components/UI/IconButton';
 
 const Stack = createNativeStackNavigator();
 const BottomTabs = createBottomTabNavigator();
 
 function ExpensesOverview() {
   return (
-    <BottomTabs.Navigator>
+    <BottomTabs.Navigator
+      screenOptions={({ navigation }) => ({
+        headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+        headerTintColor: 'white',
+        tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+        tabBarActiveTintColor: GlobalStyles.colors.accent500,
+        headerRight: ({ tinColor }) => (
+          <IconButton
+            icon="add"
+            size={24}
+            color={tinColor}
+            onPress={() => {
+              navigation.navigate('ManageExpense');
+            }}
+          ></IconButton>
+        ),
+      })}
+    >
       <BottomTabs.Screen
         name="RecentExpenses"
         component={RecentExpenses}
+        options={{
+          title: 'Recent Expenses',
+          tabBarLabel: 'Recent',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="hourglass" size={size} color={color} />
+          ),
+        }}
       ></BottomTabs.Screen>
       <BottomTabs.Screen
         name="AllExpenses"
         component={AllExpenses}
+        options={{
+          title: 'All Expenses',
+          tabBarLabel: 'All Expenses',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="calendar" size={size} color={color} />
+          ),
+        }}
       ></BottomTabs.Screen>
     </BottomTabs.Navigator>
   );
@@ -31,26 +64,26 @@ export default function App() {
     <>
       <StatusBar style="auto"></StatusBar>
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+            headerTintColor: 'white',
+          }}
+        >
           <Stack.Screen
             name="ExpensesOverview"
             component={ExpensesOverview}
+            options={{ headerShown: false }}
           ></Stack.Screen>
           <Stack.Screen
             name="ManageExpense"
             component={ManageExpense}
+            options={{
+              presentation: 'modal',
+            }}
           ></Stack.Screen>
         </Stack.Navigator>
       </NavigationContainer>
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ store/expenses-context.js
@@ -0,0 +1,92 @@
+import { createContext, useReducer } from 'react';
+
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    description: 'A pair of shoes',
+    amount: 59.99,
+    date: new Date('2021-12-19'),
+  },
+  {
+    id: 'e2',
+    description: 'A pair of trousers',
+    amount: 89.29,
+    date: new Date('2022-01-05'),
+  },
+  {
+    id: 'e3',
+    description: 'Some bananas',
+    amount: 5.99,
+    date: new Date('2021-12-01'),
+  },
+  {
+    id: 'e4',
+    description: 'A book',
+    amount: 14.99,
+    date: new Date('2022-02-19'),
+  },
+  {
+    id: 'e5',
+    description: 'Another book',
+    amount: 18.59,
+    date: new Date('2022-02-18'),
+  },
+];
+
+// context用来做外部链接，定义所有函数，方便外部使用
+export const ExpensesContext = createContext({
+  expense: [],
+  addExpense: ({ description, amount, date }) => {},
+  deleteExpense: (id) => {},
+  updateExpense: (id, { description, amount, date }) => {},
+});
+
+// reducer这个函数用来做计算
+//之类的state指的是原来的内容，action指的是传入的数据
+function expensesReducer(state, action) {
+  switch (action.type) {
+    case 'ADD':
+      const id = new Date().toString() + Math.random().toString();
+      return [{ ...action.payload, id: id }, ...state];
+    case 'UPDATE':
+      const updatableExpenseIndex = state.findIndex(
+        (expense) => expense.id === action.payload.id
+      );
+      //找到要更新的原内容
+      const updatableExpense = state[updatableExpenseIndex];
+      //把原内容加上要更新的内容变成新内容
+      const updatedItem = { ...updatableExpense, ...action.payload.data };
+      //找到原来的总内容
+      const updatedExpenses = [...state];
+      //在总内容里更新新内容
+      updatedExpenses[updatableExpenseIndex] = updatedItem;
+      return updatedExpenses;
+    case 'DELETE':
+      return state.filter((expense) => expense.id !== action.payload);
+    default:
+      return state;
+    //最后所有case计算后返回的都是state，也就是expense的数据数组（像dummy data那样的格式）
+  }
+}
+
+// context provider用来跟外部链接，要写在<app>里
+function ExpensesContextProvider({ children }) {
+  const [expenseState, dispatch] = useReducer(expensesReducer, DUMMY_EXPENSES);
+
+  //payload指的是要传入的数据
+  function addExpense(expenseData) {
+    dispatch({ type: 'ADD', payload: expenseData });
+  }
+
+  function deleteExpense(id) {
+    dispatch({ type: 'DELETE', payload: id });
+  }
+
+  function updateExpense(id, expenseData) {
+    dispatch({ type: 'UPDATE', payload: { id: id, data: expenseData } });
+  }
+
+  return <ExpensesContext.Provider>{children}</ExpensesContext.Provider>;
+}
+
+export default ExpensesContextProvider;
